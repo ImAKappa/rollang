ae56e1d8a5c81179bd3c7a3b4f858d8a027381bf
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2024-02-08 21:39:32 -0500

    Updated cli

--- crates/rollang-cli/src/main.rs
+++ crates/rollang-cli/src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rollang v0.1.0");
+
+    loop {
+        print!(">>> ");
+        io::stdout().flush().expect("Issue flushing stdout buffer");
+
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+        let command = command.trim();
+
+        if command == "end" {
+            println!("~~~ End of Session ~~~");
+            break;
+        } else {
+            println!("{command}");
+        }
+    }
 }
